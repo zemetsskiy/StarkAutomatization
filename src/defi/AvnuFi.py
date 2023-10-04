4a1ed5e3681833cdcf82749e870c75368349ee20
zemetsskiy <68808330+zemetsskiy@users.noreply.github.com>
2023-10-04 20:47:39 +0300

    Update AvnuFi.py

--- src/defi/AvnuFi.py
+++ src/defi/AvnuFi.py
@@ -82,6 +82,10 @@ class AvnuFi:
                                                     decimals=6)
                         min_amount = min_to_amount
 
+                    elif from_token_name == 'USDT' or from_token_name == 'USDC':
+                        min_to_amount = TokenAmount(amount=float(amount.Ether) * (1 - self.slippage / 100), decimals=18)
+                        min_amount = min_to_amount
+
                 elif to_token_name == 'ETH':
                     min_to_amount = TokenAmount(amount=float(amount.Ether) / eth_price * (1 - self.slippage / 100), decimals=18)
                     min_amount = min_to_amount
