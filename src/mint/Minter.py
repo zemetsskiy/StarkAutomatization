f152c98391ab1beac00d9e65b29afe70b4d8d7fb
zemetsskiy <yaroslavzhemerikin_@outlook.com>
2023-10-11 01:34:37 +0300

    mints upd

--- src/mint/Minter.py
+++ src/mint/Minter.py
@@ -19,10 +19,15 @@ class Minter:
 
             logger.info(f"[{self.client.address_to_log}] Minting nft [StarkVerse NFT]")
 
-            tx_hash = await self.client.send_transaction(interacted_contract=contract,
-                                                         function_name='publicMint',
-                                                         to=self.client.address
-                                                         )
+            # tx_hash = await self.client.send_transaction(interacted_contract=contract,
+            #                                              function_name='publicMint',
+            #                                              to=self.client.address
+            #                                              )
+
+            tx_hash = await self.client.call(interacted_contract_address=contract_address,
+                                             calldata=[self.client.address],
+                                             selector_name='publicMint')
+
             if tx_hash:
                 logger.info(
                     f"[{self.client.address_to_log}] Successfully minted [StarkVerse NFT]")
@@ -46,9 +51,13 @@ class Minter:
             id = random.randint(0, 2**128 - 1)
 
             max_fee = TokenAmount(amount=float(uniform(0.0007534534534, 0.001)))
+            #
+            # prepared_call = contract.functions["mint"].prepare(id, max_fee=int(max_fee.Wei * (1 + randint(15, 30) / 100)))
+            # await prepared_call.invoke()
 
-            prepared_call = contract.functions["mint"].prepare(id, max_fee=int(max_fee.Wei * (1 + randint(15, 30) / 100)))
-            await prepared_call.invoke()
+            tx_hash = await self.client.call(interacted_contract_address=contract_address,
+                                             calldata=[id],
+                                             selector_name='mint')
 
             logger.info(f"[{self.client.address_to_log}] Successfully minted [Starknet.id Identity NFT]")
             return True
